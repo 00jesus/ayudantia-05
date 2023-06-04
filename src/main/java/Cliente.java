0e6c77bc36595c4f37c8d6ab1890e7a7f53f57d0
00jesus <j.acevedo03@ufromail.cl>
2023-06-03 21:37:36 -0400

    Update de codigo fuente de proyecto

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,39 @@
+class Cliente {
+    private String nombre;
+    private int billetera;
+
+    public Cliente(String nombre, int billetera) {
+        this.nombre = nombre;
+        this.billetera = billetera;
+    }
+    public Cliente() {
+    }
+    public void iniciarBilleteraRandom() {
+        this.billetera = (int)(Math.random()*10001);
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getBilletera() {
+        return billetera;
+    }
+
+    public void setBilletera(int billetera) {
+        this.billetera = billetera;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nombre='" + nombre + '\'' +
+                ", billetera=" + billetera +
+                '}';
+    }
+}
---
+++ src/main/java/Te.java
@@ -0,0 +1,47 @@
+public class Te {
+    private String nombre;
+    private int precio;
+    private String categoria;
+
+    public Te(String nombre, int precio, String categoria) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
+
+    public Te() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Te{" +
+                "nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", categoria='" + categoria + '\'' +
+                '}';
+    }
+}
